cf04d31caef251626a4cff564700157c70a8d5c8
Khan <faisalshakoorkhan@gmail.com>
2018-02-02 11:20:24 +0000

    fix create, add error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,15 +5,25 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_path(@article)
+        if @article.save
+            flash[:notice] = "You have successfuly created your article."
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+        @article = Article.find(params[:id])
         
     end
     
     private
-        def article_params
-            params.require(:article).permit(:title, :description)
-            
-        end
+    
+    def article_params
+        params.require(:article).permit(:title, :description)
+        
+    end
+    
     
 end
